df8888acf9fcc8bd0cc8143b83d5ae708aef653d
Vitoria Maia <97573142+vitoriadev@users.noreply.github.com>
2022-03-31 15:49:54 -0300

    mentalista

---
+++ test.js
@@ -0,0 +1,21 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados.
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let x = parseFloat(gets(100.00)
+
+//Complete os espaços em branco com uma possível solução para o problema
+   if ((x > 0) && (x <= 25)){
+    print("Intervalo [0, 25]");
+   } else if ((x >= 25) && (x <= 50)){
+     print("Intervalo [25, 50]");
+   } else if ((x >= 50) && (x <= 75)){
+     print("Intervalo [50, 75]");
+   } else if ((x >= 75) && (x <= 100)){
+     print("Intervalo [75, 100]");
+   } else {
+     print("Fora de Intervalo")
+   }
+     
+
+    
+
